b20e0c11e5c38d8051de7e20857d2d271bbef383
Malik Kilgore <malik199228@gmail.com>
2020-04-01 17:24:11 -0400

    Fixed bug with extra square being created

--- script.js
+++ script.js
@@ -14,64 +14,83 @@ const gameBoard = (() => {
             let boardSquare = document.createElement('div');
             boardSquare.className = 'boardSquare';
             boardSquare.id = `square${loopnumber}`;
-            boardSquare.addEventListener('hover', function(e){
-                
+            boardSquare.addEventListener('mouseover', function(e){
+                boardSquare.style.backgroundColor = 'grey';
+
+            });
+            boardSquare.addEventListener('mouseout', function(e){
+                boardSquare.style.backgroundColor = 'white';
             });
             boardSquare.addEventListener('click', function(e){
-                // renderGrid()
+                // gameBoard.renderGrid()
             });
             // v This is temporary for testing
+
             switch(loopnumber) {
                 case 1:
                     let square0 = document.getElementById(`square0`)
-                    array[0] = 'X'                    
+                    array[0] = '0: X'        
+                    boardContainer.appendChild(boardSquare);            
                     square0.innerHTML = array[0]
                     break
                 case 2:
                     let square1 = document.getElementById(`square1`)
-                    array[1] = 'O'
+                    array[1] = '1: O'
+                    boardContainer.appendChild(boardSquare);
                     square1.innerHTML = array[1]
+                    
                     break
                 case 3:
                     let square2 = document.getElementById(`square2`)
-                    array[2] = 'X'              
+                    array[2] = '2: X'    
+                    boardContainer.appendChild(boardSquare);          
                     square2.innerHTML = array[2]
                     break
                 case 4:
                     let square3 = document.getElementById(`square3`)
-                    array[3] = 'O'
+                    array[3] = '3: O'
+                    boardContainer.appendChild(boardSquare);
                     square3.innerHTML = array[3]
                     break
                 case 5:
                     let square4 = document.getElementById(`square4`)
-                    array[4] = 'X'
+                    array[4] = '4: X'
+                    boardContainer.appendChild(boardSquare);
                     square4.innerHTML = array[4]
                     break
                 case 6:
                     let square5 = document.getElementById(`square5`)
-                    array[5] = 'O'
+                    array[5] = '5: O'
+                    boardContainer.appendChild(boardSquare);
                     square5.innerHTML = array[5]
                     break
                 case 7:
                     let square6 = document.getElementById(`square6`)
-                    array[6] = 'X'
+                    array[6] = '6: X'
+                    boardContainer.appendChild(boardSquare);
                     square6.innerHTML = array[6]
                     break
                 case 8:
                     let square7 = document.getElementById(`square7`)
-                    array[7] = 'O'
+                    array[7] = '7: O'
+                    boardContainer.appendChild(boardSquare);
                     square7.innerHTML = array[7]
                     break
                 case 9:
                     let square8 = document.getElementById(`square8`)
-                    array[8] = 'X'
+                    array[8] = '8: X'
+                    boardContainer.appendChild(boardSquare);
                     square8.innerHTML = array[8]
                     break
     
             };
 
             boardContainer.appendChild(boardSquare);
+
         };
+        //outside for loop. Removes extra square.
+        let square9 = document.getElementById('square9')
+        square9.remove()
     };
 
     let renderGrid = function() {
@@ -79,41 +98,50 @@ const gameBoard = (() => {
 
         switch(loopnumber) {
             case 1:
-                let square1 = document.getElementById('square1')
-                square1.innerHTML = array[0]
-                return array[0] = 'X'
+                let square0 = document.getElementById(`square0`)
+                array[0] = 'X'                    
+                square0.innerHTML = array[0]
+                break
             case 2:
-                let square2 = document.getElementById('square2')
-                square2.innerHTML = array[1]
-                return array[1] = 'O'
+                let square1 = document.getElementById(`square1`)
+                array[1] = 'O'
+                square1.innerHTML = array[1]
+                break
             case 3:
-                let square3 = document.getElementById('square3')
-                square3.innerHTML = array[2]
-                return array[2] = 'X'
+                let square2 = document.getElementById(`square2`)
+                array[2] = 'X'              
+                square2.innerHTML = array[2]
+                break
             case 4:
-                let square4 = document.getElementById('square4')
-                square4.innerHTML = array[3]
-                return array[3] = 'O'
+                let square3 = document.getElementById(`square3`)
+                array[3] = 'O'
+                square3.innerHTML = array[3]
+                break
             case 5:
-                let square5 = document.getElementById('square5')
-                square5.innerHTML = array[4]
-                return array[4] = 'X'
+                let square4 = document.getElementById(`square4`)
+                array[4] = 'X'
+                square4.innerHTML = array[4]
+                break
             case 6:
-                let square6 = document.getElementById('square6')
-                square6.innerHTML = array[5]
-                return array[5] = 'O'
+                let square5 = document.getElementById(`square5`)
+                array[5] = 'O'
+                square5.innerHTML = array[5]
+                break
             case 7:
-                let square7 = document.getElementById('square7')
-                square7.innerHTML = array[6]
-                return array[6] = 'X'
+                let square6 = document.getElementById(`square6`)
+                array[6] = 'X'
+                square6.innerHTML = array[6]
+                break
             case 8:
-                let square8 = document.getElementById('square8')
-                square8.innerHTML = array[7]
-                return array[7] = 'O'
+                let square7 = document.getElementById(`square7`)
+                array[7] = 'O'
+                square7.innerHTML = array[7]
+                break
             case 9:
-                let square9 = document.getElementById('square9')
-                square9.innerHTML = array[8]
-                return array[8] = 'X'
+                let square8 = document.getElementById(`square8`)
+                array[8] = 'X'
+                square8.innerHTML = array[8]
+                break
 
         };
 
