3ecc646d7415664d77ac4e0400a0112226d48bbd
Malik Kilgore <malik199228@gmail.com>
2020-04-02 00:01:31 -0400

    Edited playerFactory. Set groundwork for linking DOM elements to Objects in JS

--- script.js
+++ script.js
@@ -5,141 +5,75 @@ const gameBoard = (() => {
     let createGrid = function() {
 
         let boardContainer = document.getElementById('boardContainer');
-        
-        for (let i = 0; i <= 9; i++) {
+
+        //Generates the grid
+        for (let i = 0; i < 9; i++) {
+
             let loopnumber = i
-
-            console.log(loopnumber)
-
             let boardSquare = document.createElement('div');
+
             boardSquare.className = 'boardSquare';
             boardSquare.id = `square${loopnumber}`;
+
             boardSquare.addEventListener('mouseover', function(e){
                 boardSquare.style.backgroundColor = 'grey';
-
             });
             boardSquare.addEventListener('mouseout', function(e){
                 boardSquare.style.backgroundColor = 'white';
             });
             boardSquare.addEventListener('click', function(e){
-                // gameBoard.renderGrid()
+                //display.playerSensor(playerOne, playerTwo)
+                //Change Obj innerHTML
             });
-            // v This is temporary for testing
-
-            switch(loopnumber) {
-                case 1:
-                    let square0 = document.getElementById(`square0`)
-                    array[0] = '0: X'        
-                    boardContainer.appendChild(boardSquare);            
-                    square0.innerHTML = array[0]
-                    break
-                case 2:
-                    let square1 = document.getElementById(`square1`)
-                    array[1] = '1: O'
-                    boardContainer.appendChild(boardSquare);
-                    square1.innerHTML = array[1]
-                    
-                    break
-                case 3:
-                    let square2 = document.getElementById(`square2`)
-                    array[2] = '2: X'    
-                    boardContainer.appendChild(boardSquare);          
-                    square2.innerHTML = array[2]
-                    break
-                case 4:
-                    let square3 = document.getElementById(`square3`)
-                    array[3] = '3: O'
-                    boardContainer.appendChild(boardSquare);
-                    square3.innerHTML = array[3]
-                    break
-                case 5:
-                    let square4 = document.getElementById(`square4`)
-                    array[4] = '4: X'
-                    boardContainer.appendChild(boardSquare);
-                    square4.innerHTML = array[4]
-                    break
-                case 6:
-                    let square5 = document.getElementById(`square5`)
-                    array[5] = '5: O'
-                    boardContainer.appendChild(boardSquare);
-                    square5.innerHTML = array[5]
-                    break
-                case 7:
-                    let square6 = document.getElementById(`square6`)
-                    array[6] = '6: X'
-                    boardContainer.appendChild(boardSquare);
-                    square6.innerHTML = array[6]
-                    break
-                case 8:
-                    let square7 = document.getElementById(`square7`)
-                    array[7] = '7: O'
-                    boardContainer.appendChild(boardSquare);
-                    square7.innerHTML = array[7]
-                    break
-                case 9:
-                    let square8 = document.getElementById(`square8`)
-                    array[8] = '8: X'
-                    boardContainer.appendChild(boardSquare);
-                    square8.innerHTML = array[8]
-                    break
-    
-            };
 
             boardContainer.appendChild(boardSquare);
 
         };
-        //outside for loop. Removes extra square.
-        let square9 = document.getElementById('square9')
-        square9.remove()
+
     };
+    //OBJECT Constructor here
+    let makeObject = function() {
 
-    let renderGrid = function() {
-        // array goes through array[0-8]. Temporary logic.
+    }
 
-        switch(loopnumber) {
+    let renderGrid = function(boardSquare) {
+        // array goes through array[0-8]. TEMPORARY
+
+        switch(boardSquare) {
             case 1:
-                let square0 = document.getElementById(`square0`)
-                array[0] = 'X'                    
+                array[0] = '0: X'                  
                 square0.innerHTML = array[0]
                 break
             case 2:
-                let square1 = document.getElementById(`square1`)
-                array[1] = 'O'
+                array[1] = '1: O'
                 square1.innerHTML = array[1]
                 break
             case 3:
-                let square2 = document.getElementById(`square2`)
-                array[2] = 'X'              
+                array[2] = '2: X'             
                 square2.innerHTML = array[2]
                 break
             case 4:
-                let square3 = document.getElementById(`square3`)
-                array[3] = 'O'
+                array[3] = '3: O'
                 square3.innerHTML = array[3]
                 break
             case 5:
-                let square4 = document.getElementById(`square4`)
-                array[4] = 'X'
+                array[4] = '4: X'
                 square4.innerHTML = array[4]
                 break
             case 6:
-                let square5 = document.getElementById(`square5`)
-                array[5] = 'O'
+                array[5] = '5: O'
                 square5.innerHTML = array[5]
                 break
             case 7:
-                let square6 = document.getElementById(`square6`)
-                array[6] = 'X'
+                array[6] = '6: X'
                 square6.innerHTML = array[6]
                 break
             case 8:
-                let square7 = document.getElementById(`square7`)
-                array[7] = 'O'
+                array[7] = '7: O'
                 square7.innerHTML = array[7]
                 break
             case 9:
-                let square8 = document.getElementById(`square8`)
-                array[8] = 'X'
+                array[8] = '8: X'
                 square8.innerHTML = array[8]
                 break
 
@@ -147,21 +81,98 @@ const gameBoard = (() => {
 
     };
 
-    return {array, createGrid};
+    return {array, createGrid, renderGrid};
     
 })()
 
-const displayController =(() => {
+const display =(() => {
+    
+    const thisGrid = {
+        /*These will be converted into objects. id will be stored,
+        array[] association will be stored.
+        eventListener will change innerHTML depending on which player clicked.
+        Can possibly just make it so these objects do the work.
+        Create constructor in gameBoard */
+        square0 = document.getElementById(`square0`),              
+
+        square1 = document.getElementById(`square1`),
+
+        square2 = document.getElementById(`square2`),
+
+        square3 = document.getElementById(`square3`),
+
+        square4 = document.getElementById(`square4`),
+
+        square5 = document.getElementById(`square5`),
+
+        square6 = document.getElementById(`square6`),
+
+        square7 = document.getElementById(`square7`),
+
+        square8 = document.getElementById(`square8`),
+    };
+
+    /*
+    On click, run displayController.nodeSensor
+    Current node is returned as square0-8
+    playerSensor() runs, determines marked player. 
+    Determines which node. Changes node innerHTML
+    */
+
+    let nodeSensor = () => {
+
+        switch(node){
+            case square0:
+                return node = square0
+            
+        }
+
+
+
+    };
+    
+    //Senses which player is currently clicking
+    let playerSensor = () => {
+        switch(playerOne.marker){
+            //temp basic examples. both evals need to also look at current square.
+            case true:
+                array[0] = 'X'
+                square0.innerHTML = array[0]
+            case false:
+                array[0] = 'O'
+                square0.innerHTML = array[0]
+        };
+    };
+
+    return {playerSensor, nodeSensor}
 
 })()
 
 //Factories
-const playerFactory = (name) => {
-    const leftClick = () => console.log('temporary left click');
+const playerFactory = (name, marker) => {
 
-    return {leftClick, name};
-}
-const playerOne = playerFactory('Temp1')
-const PlayerTwo = playerFactory('Temp2')
+    let leftClick = () => {console.log('temporary left click')};
 
+    this.marker = marker;
+
+    let markerChange = () => {
+        switch(marker){
+            case false:
+                marker = true
+                break
+            case true:
+                marker = false
+                break
+        };
+    };
+
+    return {leftClick, name, marker, markerChange};
+};
+//Objects
+const playerOne = playerFactory('Temp1', true)
+const playerTwo = playerFactory('Temp2', false)
+
+
+console.log(playerOne.marker)
+console.log(playerTwo.marker)
 gameBoard.createGrid()
