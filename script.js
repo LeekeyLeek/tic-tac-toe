6658868c2c093b372ff329c6e5d220f6df572fa6
Malik Kilgore <malik199228@gmail.com>
2020-04-02 23:32:48 -0400

    Linked JS to DOM Elements with nodeSensor() and playerSensor().

--- script.js
+++ script.js
@@ -22,8 +22,8 @@ const gameBoard = (() => {
                 boardSquare.style.backgroundColor = 'white';
             });
             boardSquare.addEventListener('click', function(e){
-                //display.playerSensor(playerOne, playerTwo)
-                //Change Obj innerHTML
+                display.nodeSensor(boardSquare)
+                display.playerSensor(node)
             });
 
             boardContainer.appendChild(boardSquare);
@@ -31,130 +31,205 @@ const gameBoard = (() => {
         };
 
     };
-    //OBJECT Constructor here
-    let makeObject = function() {
 
-    }
-
-    let renderGrid = function(boardSquare) {
-        // array goes through array[0-8]. TEMPORARY
-
-        switch(boardSquare) {
-            case 1:
-                array[0] = '0: X'                  
-                square0.innerHTML = array[0]
-                break
-            case 2:
-                array[1] = '1: O'
-                square1.innerHTML = array[1]
-                break
-            case 3:
-                array[2] = '2: X'             
-                square2.innerHTML = array[2]
-                break
-            case 4:
-                array[3] = '3: O'
-                square3.innerHTML = array[3]
-                break
-            case 5:
-                array[4] = '4: X'
-                square4.innerHTML = array[4]
-                break
-            case 6:
-                array[5] = '5: O'
-                square5.innerHTML = array[5]
-                break
-            case 7:
-                array[6] = '6: X'
-                square6.innerHTML = array[6]
-                break
-            case 8:
-                array[7] = '7: O'
-                square7.innerHTML = array[7]
-                break
-            case 9:
-                array[8] = '8: X'
-                square8.innerHTML = array[8]
-                break
-
-        };
-
-    };
-
-    return {array, createGrid, renderGrid};
+    return {array, createGrid};
     
 })()
 
 const display =(() => {
     
-    const thisGrid = {
-        /*These will be converted into objects. id will be stored,
-        array[] association will be stored.
-        eventListener will change innerHTML depending on which player clicked.
-        Can possibly just make it so these objects do the work.
-        Create constructor in gameBoard */
-        square0 = document.getElementById(`square0`),              
+    var square0 = document.getElementById(`square0`)    
 
-        square1 = document.getElementById(`square1`),
+    var square1 = document.getElementById(`square1`)
 
-        square2 = document.getElementById(`square2`),
+    var square2 = document.getElementById(`square2`)
 
-        square3 = document.getElementById(`square3`),
+    var square3 = document.getElementById(`square3`)
 
-        square4 = document.getElementById(`square4`),
+    var square4 = document.getElementById(`square4`)
 
-        square5 = document.getElementById(`square5`),
+    var square5 = document.getElementById(`square5`)
 
-        square6 = document.getElementById(`square6`),
+    var square6 = document.getElementById(`square6`)
 
-        square7 = document.getElementById(`square7`),
+    var square7 = document.getElementById(`square7`)
 
-        square8 = document.getElementById(`square8`),
-    };
+    var square8 = document.getElementById(`square8`)
 
-    /*
-    On click, run displayController.nodeSensor
-    Current node is returned as square0-8
-    playerSensor() runs, determines marked player. 
-    Determines which node. Changes node innerHTML
-    */
+    //Identifies the current node
+    let nodeSensor = (boardSquare) => {
 
-    let nodeSensor = () => {
+        switch(boardSquare.id){
+            case "square0":
+                console.log('node is now set to square0')
+                return node = display.square0
+            case "square1":
+                console.log('node is now set to square1')
+                return node = display.square1
+            case "square2":
+                console.log('node is now set to square2')
+                return node = display.square2
+            case "square3":
+                console.log('node is now set to square3')
+                return node = display.square3
+            case "square4":
+                console.log('node is now set to square4')
+                return node = display.square4
+            case "square5":
+                console.log('node is now set to square5')
+                return node = display.square5
+            case "square6":
+                console.log('node is now set to square6')
+                return node = display.square6
+            case "square7":
+                console.log('node is now set to square7')
+                return node = display.square7
+            case "square8":
+                console.log('node is now set to square8')
+                return node = display.square8
 
-        switch(node){
-            case square0:
-                return node = square0
-            
-        }
-
-
+        };
 
     };
     
-    //Senses which player is currently clicking
+    //Senses which player is currently clicking and returns their answer.
     let playerSensor = () => {
+
         switch(playerOne.marker){
-            //temp basic examples. both evals need to also look at current square.
+
             case true:
-                array[0] = 'X'
-                square0.innerHTML = array[0]
+                console.log('The node is ' + node)
+                switch(node){
+                    case square0:
+                        gameBoard.array[0] = 'X'
+                        document.getElementById(`square0`).innerHTML = gameBoard.array[0]
+                        playerOne.markerChange()
+                        playerTwo.markerChange()
+                        break
+                    case square1:
+                        gameBoard.array[1] = 'X'
+                        document.getElementById(`square1`).innerHTML = gameBoard.array[1]
+                        playerOne.markerChange()
+                        playerTwo.markerChange()
+                        break
+                    case square2:
+                        gameBoard.array[2] = 'X'
+                        document.getElementById(`square2`).innerHTML = gameBoard.array[2]
+                        playerOne.markerChange()
+                        playerTwo.markerChange()
+                        break
+                    case square3:
+                        gameBoard.array[3] = 'X'
+                        document.getElementById(`square3`).innerHTML = gameBoard.array[3]
+                        playerOne.markerChange()
+                        playerTwo.markerChange()
+                        break
+                    case square4:
+                        gameBoard.array[4] = 'X'
+                        document.getElementById(`square4`).innerHTML = gameBoard.array[4]
+                        playerOne.markerChange()
+                        playerTwo.markerChange()
+                        break
+                    case square5:
+                        gameBoard.array[5] = 'X'
+                        document.getElementById(`square5`).innerHTML = gameBoard.array[5]
+                        playerOne.markerChange()
+                        playerTwo.markerChange()
+                        break
+                    case square6:
+                        gameBoard.array[6] = 'X'
+                        document.getElementById(`square6`).innerHTML = gameBoard.array[6]
+                        playerOne.markerChange()
+                        playerTwo.markerChange()
+                        break
+                    case square7:
+                        gameBoard.array[7] = 'X'
+                        document.getElementById(`square7`).innerHTML = gameBoard.array[7]
+                        playerOne.markerChange()
+                        playerTwo.markerChange()
+                        break
+                    case square8:
+                        gameBoard.array[8] = 'X'
+                        document.getElementById(`square8`).innerHTML = gameBoard.array[8]
+                        playerOne.markerChange()
+                        playerTwo.markerChange()
+                        break
+                };
+          
             case false:
-                array[0] = 'O'
-                square0.innerHTML = array[0]
+                console.log('The node is ' + node)
+                switch(node){
+                    case square0:
+                        gameBoard.array[0] = 'O'
+                        document.getElementById(`square0`).innerHTML = gameBoard.array[0]
+                        playerOne.markerChange()
+                        playerTwo.markerChange()
+                        break
+                    case square1:
+                        gameBoard.array[1] = 'O'
+                        document.getElementById(`square1`).innerHTML = gameBoard.array[1]
+                        playerOne.markerChange()
+                        playerTwo.markerChange()
+                        break
+                    case square2:
+                        gameBoard.array[2] = 'O'
+                        document.getElementById(`square2`).innerHTML = gameBoard.array[2]
+                        playerOne.markerChange()
+                        playerTwo.markerChange()
+                        break
+                    case square3:
+                        gameBoard.array[3] = 'O'
+                        document.getElementById(`square3`).innerHTML = gameBoard.array[3]
+                        playerOne.markerChange()
+                        playerTwo.markerChange()
+                        break
+                    case square4:
+                        gameBoard.array[4] = 'O'
+                        document.getElementById(`square4`).innerHTML = gameBoard.array[4]
+                        playerOne.markerChange()
+                        playerTwo.markerChange()
+                        break
+                    case square5:
+                        gameBoard.array[5] = 'O'
+                        document.getElementById(`square5`).innerHTML = gameBoard.array[5]
+                        playerOne.markerChange()
+                        playerTwo.markerChange()
+                        break
+                    case square6:
+                        gameBoard.array[6] = 'O'
+                        document.getElementById(`square6`).innerHTML = gameBoard.array[6]
+                        playerOne.markerChange()
+                        playerTwo.markerChange()
+                        break
+                    case square7:
+                        gameBoard.array[7] = 'O'
+                        document.getElementById(`square7`).innerHTML = gameBoard.array[7]
+                        playerOne.markerChange()
+                        playerTwo.markerChange()
+                        break
+                    case square8:
+                        gameBoard.array[8] = 'O'
+                        document.getElementById(`square8`).innerHTML = gameBoard.array[8]
+                        playerOne.markerChange()
+                        playerTwo.markerChange()
+                        break
+                };
         };
+        console.log('Player one is set to ' + playerOne.marker)
+        console.log('Player two is set to ' + playerTwo.marker)
     };
 
-    return {playerSensor, nodeSensor}
+    return {playerSensor, nodeSensor, square0,square1,square2,square3,square4,
+        square5, square6, square7, square8}
 
 })()
 
 //Factories
 const playerFactory = (name, marker) => {
 
-    let leftClick = () => {console.log('temporary left click')};
+    marker = marker;
 
-    this.marker = marker;
-
+    //This runs after playerSensor()
     let markerChange = () => {
         switch(marker){
             case false:
@@ -166,13 +241,11 @@ const playerFactory = (name, marker) => {
         };
     };
 
-    return {leftClick, name, marker, markerChange};
+    return {name, marker, markerChange};
 };
+
 //Objects
 const playerOne = playerFactory('Temp1', true)
 const playerTwo = playerFactory('Temp2', false)
 
-
-console.log(playerOne.marker)
-console.log(playerTwo.marker)
 gameBoard.createGrid()
